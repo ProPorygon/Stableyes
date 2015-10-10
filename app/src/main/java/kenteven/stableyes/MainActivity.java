204d647c0257771a3816e7b3a523a0b57f48ad9a
Aditya Sriram <aditsri@gmail.com>
2015-10-10 01:36:53 -0500

    added image

--- app/src/main/java/kenteven/stableyes/MainActivity.java
+++ app/src/main/java/kenteven/stableyes/MainActivity.java
@@ -5,9 +5,10 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +27,10 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        ImageView img = (ImageView) findViewById(R.id.imageView);
+        img.setImageResource(R.drawable.random);
+
     }
 
     @Override
